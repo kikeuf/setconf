1568a4ed80c1ba4f5196db0d7592e94d7153bde6
kikeuf <kikeuf@free.fr>
2022-10-14 15:22:54 +0200

    environment issue

--- src/environment.py
+++ src/environment.py
@@ -1,12 +1,23 @@
 
 import os
+from sys import platform
 
 def setenvvar(varname, value):
    try:
-       #os.environ[varname] = value
-       os.environ.setdefault(varname, value)
-       #os.system('export ' + varname + '=' + value)
-       return True
+
+        print('set: $' + varname + '=' + value)
+
+
+        if platform == "linux" or platform == "linux2":
+            os.system('export ' + varname + '=' + value)
+        else:
+            os.environ.setdefault(varname, value)
+            # os.environ[varname] = value
+
+        rvalue = os.environ.get(varname)
+        print('check: $' + varname + '=' + rvalue)
+
+        return True
 
    except Exception:
        return False
