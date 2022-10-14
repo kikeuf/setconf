f1ad0c73bb71c9bfbaeae8d4d6c5ff48cdbe28e8
kikeuf <kikeuf@free.fr>
2022-10-14 11:17:09 +0200

    chmod

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from sys import platform
 
 if platform == "linux" or platform == "linux2":
-    os.system('sudo chmod -x setconf')
+    os.system('sudo chmod -x ./src/setconf/setconf')
     #find . -type f -exec chmod -x setconf \;
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
