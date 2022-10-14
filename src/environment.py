6d2f609eb9d6bb52f38cd44296d33b5b2b7eed77
kikeuf <kikeuf@free.fr>
2022-10-14 18:52:15 +0200

    environment issue

--- src/environment.py
+++ src/environment.py
@@ -5,28 +5,28 @@ from sys import platform
 def setenvvar(varname, value):
    try:
 
-        print('set: $' + varname + '=' + value)
-
-
         if platform == "linux" or platform == "linux2":
-            os.system('export ' + varname + '=' + value)
+            #os.system('export ' + varname + '=' + value)
+            os.environ.setdefault(varname, value)
         else:
             os.environ.setdefault(varname, value)
             # os.environ[varname] = value
 
-        rvalue = os.environ.get(varname)
-        print('check: $' + varname + '=' + rvalue)
+        #rvalue = os.environ.get(varname)
+        #print('check: $' + varname + '=' + rvalue)
 
+        writefile(varname, value)
         return True
 
-   except Exception:
-       return False
-
+   except Exception as e:
+       return "error : " + repr(e)
 
 def getenvvar(varname):
     try:
         #value=os.environ[varname]
         value = os.environ.get(varname)
+        if value == "":
+            value = readfile(varname)
         return value
 
     except Exception:
@@ -41,3 +41,38 @@ def envvarexists(varname):
 
     except Exception:
         return False
+
+def writefile(filename, text, erase_before = False):
+
+    try:
+
+        if erase_before:
+            deletefile(filename)
+
+        f = open(filename, 'w')
+        # Writing a string to file
+        f.write(text)
+        f.close()
+
+        return True
+
+    except:
+        return False
+
+def deletefile(filename):
+    try:
+        os.remove(filename)
+        return True
+
+    except:
+        return False
+
+def readfile(filename):
+
+    try:
+        f = open(filename, 'r')
+        ret = f.readline()
+        return ret
+
+    except:
+        return ""
