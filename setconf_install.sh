08b835b7928bfdb68c5f480f83e9b816a94126df
kikeuf <kikeuf@free.fr>
2022-10-30 03:49:28 +0100

    uninstall

--- setconf_install.sh
+++ setconf_install.sh
@@ -1,4 +1,4 @@
-#wget https://raw.githubusercontent.com/kikeuf/setconf/master/setconf_install.sh && sh setconf_install.sh && rm setconf_install.sh
+#sudo wget -P /tmp https://raw.githubusercontent.com/kikeuf/setconf/master/setconf_install.sh && sudo sh /tmp/setconf_install.sh && sudo rm /tmp/setconf_install.sh
 
 echo "Installing setconf and setdhcp"
 
@@ -6,6 +6,36 @@ echo "Installing setconf and setdhcp"
 cd /
 
 sudo pip install -e git+https://github.com/kikeuf/setconf#egg=setconf
+
 export SETCONF_PATH=`pip show setconf | grep -E "Location:" | cut -c 11-`
-sudo python3 $SETCONF_PATH/src/setconf.py -init
+export SETCONF_FILE=$SETCONF_PATH/setconf
+export SETDHCP_FILE=$SETCONF_PATH/setdhcp
+
+alias setconf=$SETCONF_FILE
+alias setdhcp=$SETDHCP_FILE
+
+sudo echo 'python3 '$SETCONF_PATH'/src/setconf.py $*' > $SETCONF_FILE
+sudo echo 'python3 '$SETCONF_PATH'/src/setconf.py -w -t dhcp $*' > $SETDHCP_FILE
+
+sudo chmod +x $SETCONF_FILE
+sudo chmod +x $SETDHCP_FILE
+
+#sudo chmod +x $SETCONF_PATH/setconf_install.sh
+sudo rm $SETCONF_PATH/setconf_install.sh
+sudo chmod +x $SETCONF_PATH/setconf_uninstall.sh
+
+#La ligne suivante ne fonctionne pas, il faut transister par un fichier temporaire comme workaround
+#export FMT_PATH=`echo $SETCONF_PATH | sed 's/\//\\\//g'`
+sudo echo $SETCONF_PATH | sed 's/\//\\\//g' > /tmp/fmt_path
+export FMT_PATH="`cat /tmp/fmt_path`"
+sudo rm /tmp/fmt_path
+
+export NEW_PATH=`echo $PATH | sed 's/'$FMT_PATH'//' | sed 's/::/:/g' | sed 's/^://' | sed 's/:$//'`
+export PATH=`echo $SETCONF_PATH':'$NEW_PATH`
+
+sudo echo 'alias setconf='$SETCONF_FILE > /etc/profile.d/00-setconf_aliases.sh
+sudo echo 'alias setdhcp='$SETDHCP_FILE >> /etc/profile.d/00-setconf_aliases.sh
+sudo echo 'export $PATH='$PATH >> /etc/profile.d/00-setconf_aliases.sh
+
+#sudo python3 $SETCONF_PATH/src/setconf.py -init
 
--- setconf_uninstall.sh
+++ setconf_uninstall.sh
@@ -8,4 +8,12 @@ sudo rm /etc/profile.d/00-setconf_aliases.sh
 export SETCONF_PATH=`pip show setconf | grep -E "Location:" | cut -c 11-`
 sudo pip uninstall git+https://github.com/kikeuf/setconf#egg=setconf
 
+#La ligne suivante ne fonctionne pas, il faut transister par un fichier temporaire comme workaround
+#export FMT_PATH=`echo $SETCONF_PATH | sed 's/\//\\\//g'`
+sudo echo $SETCONF_PATH | sed 's/\//\\\//g' > /tmp/fmt_path
+export FMT_PATH="`cat /tmp/fmt_path`"
+sudo rm /tmp/fmt_path
+
+export PATH=`echo $PATH | sed 's/'$FMT_PATH'//' | sed 's/::/:/g' | sed 's/^://' | sed 's/:$//'`
+
 sudo rm -r $SETCONF_PATH
